de12d60f27808fa084f2f56bd29c65941f93ec11
Kaio Aime Garcia <kaiogarcia@employer.com.br>
2024-04-16 14:19:06 -0300

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import { ToastContainer } from "react-toastify";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase/firebaseConection";
 import { Feed } from "./pages/Feed";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import UserEmailContext from "./contexts/UserEmail";
 
 function App() {
   const navigate = useNavigate();
+  const [userEmail, setUserEmail] = useState("");
 
   const checkLogin = () => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
+        user.providerData[0].email !== null &&
+          setUserEmail(user.providerData[0].email);
         navigate(RoutesPaths.Feed);
       } else {
         navigate(RoutesPaths.Login);
@@ -25,21 +29,21 @@ function App() {
   }, []);
 
   return (
-    <>
+    <UserEmailContext.Provider value={{ email: userEmail }}>
       <Routes>
         <Route path={RoutesPaths.Login} element={<Login />} />
         <Route path={RoutesPaths.Feed} element={<Feed />} />
       </Routes>
       <ToastContainer
         position="top-right"
-        autoClose={3000}
+        autoClose={3500}
         closeOnClick
         pauseOnFocusLoss
         pauseOnHover
         draggable
         theme="colored"
       />
-    </>
+    </UserEmailContext.Provider>
   );
 }
 
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import { Navbar } from "../components/Navbar/Navbar";
-
+import { Post } from "../models/interfaces/Post";
+import { DocumentData, collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase/firebaseConection";
+import { CollectionsFirebase } from "../models/enums/collectionsFirebase";
+import { Posts } from "../components/Posts";
 function Feed() {
+  const [postsList, setPostsList] = useState<Array<Post>>([]);
+
+  const handleGetPosts = async (): Promise<void> => {
+    const postsArray: Array<DocumentData> = [];
+    const postsListCollection = collection(db, CollectionsFirebase.POSTS);
+
+    await getDocs(postsListCollection).then((response) => {
+      if (response) {
+        for (const post of response.docs) {
+          postsArray.push(post.data());
+        }
+
+        postsArray.length > 0 && setPostsList(postsArray as Array<Post>);
+        console.log("LISTA DE POSTS", postsList);
+      }
+    });
+  };
+
+  useEffect(() => {
+    void handleGetPosts();
+  }, []);
+
   return (
     <>
-      <Navbar />
+      <Navbar handleGetPosts={handleGetPosts} />
+      <Posts posts={postsList} />
     </>
   );
 }
